f474209556790aa555729cc553953e54faa989bd
Berillo Anton <anberg137@gmail.com>
2021-11-08 20:54:25 +0700

    Merge branch 'git-merge'

--- branching/rebase.sh
+++ branching/rebase.sh
@@ -1,8 +1,8 @@
-	#!/bin/bash
-	# display command line options
+#!/bin/bash
+# display command line options
 count=1
 for param in "$@"; do
-    echo "\$@ Parameter #$count = $param"
-    count=$(( $count + 1 ))
+	echo "Next parameter: $param"    
+	count=$(( $count + 1 ))
 done
 
